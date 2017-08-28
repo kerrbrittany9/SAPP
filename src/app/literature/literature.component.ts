f72dd9984254cd422b7b8e9348a20c7273041996
Larry J. Taylor, Michelle Poterek and Max Scher <michelle@>
2017-08-28 10:46:34 -0700

    add literature component along with separate files for separate api-key configuration code

---
+++ src/app/literature/literature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-literature',
+  templateUrl: './literature.component.html',
+  styleUrls: ['./literature.component.css']
+})
+export class LiteratureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
