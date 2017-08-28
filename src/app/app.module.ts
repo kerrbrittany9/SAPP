5d5a8c8f460eb359440b8583b468ec82e23717f1
Larry J. Taylor, Michelle Poterek and Max Scher <larry@>
2017-08-28 10:45:12 -0700

    incorporate firebase and googlebooks api imports into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+
+import { masterGoogleBooksConfig } from './api-key-google';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { LiteratureComponent } from './literature/literature.component';
+
+export const googleBooksConfig = {
+  apiKey: masterGoogleBooksConfig.apiKey,
+}
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LiteratureComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
