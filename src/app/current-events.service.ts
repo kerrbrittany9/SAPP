20a80bf0428d32b2b8ce8f263c4b29fadbbc8e7f
Larry <larryjamestaylor@gmail.com>
2017-08-29 11:56:34 -0700

    move api call to service file

--- src/app/current-events.service.ts
+++ src/app/current-events.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CurrentEventsService {
 
   constructor(private http: Http) { }
 
-  getNews(stringToCall: string) {
-    return this.http.get(stringToCall);
+  getBySource(source: string) {
+    return this.http.get('https://newsapi.org/v1/articles?source='+ source + '&apikey=54c70d579915400ebe7b9d7c36f6fb77');
   }
 }
