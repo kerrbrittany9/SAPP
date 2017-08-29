34319adc23253579316996f039fe7ea404b5438b
Larry <larryjamestaylor@gmail.com>
2017-08-29 11:58:33 -0700

    add @Input

--- src/app/current-events-list/current-events-list.component.ts
+++ src/app/current-events-list/current-events-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import{ masterNewsApiConfig } from '../api-key-current-events';
+import { CurrentEventsService } from '../current-events.service';
+import { CurrentEventsComponent } from '../current-events/current-events.component';
 
 @Component({
   selector: 'app-current-events-list',
   templateUrl: './current-events-list.component.html',
-  styleUrls: ['./current-events-list.component.css']
+  styleUrls: ['./current-events-list.component.css'],
+  providers: [CurrentEventsService]
 })
-export class CurrentEventsListComponent implements OnInit {
+export class CurrentEventsListComponent {
+@Input() childArticles;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private currentEventsService: CurrentEventsService) { }
 
 }
