f98c00d290c3b49bc887ec6cd3101726b5fbd343
Calla Rudolph and Larry Taylor <calla@>
2017-08-31 10:56:51 -0700

    add delete funcionality to saved trivia items

--- src/app/saved-cities/saved-cities.component.ts
+++ src/app/saved-cities/saved-cities.component.ts
@@ -25,7 +25,6 @@ export class SavedCitiesComponent implements OnInit {
   }
 
   sendCityToEvent(event: Event, city: CityConversation) {
-    // console.log(event);
     event.conversations.push(city.id);
     this.eventService.saveCityToEvent(event);
   }
