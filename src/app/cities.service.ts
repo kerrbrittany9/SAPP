15a3f9fb344381a1db09ddd12cd9db25cf63bf9a
Calla Rudolph and Larry Taylor <calla@>
2017-08-31 09:34:43 -0700

    refactor city conversations to save with a city name and ID

---
+++ src/app/cities.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { CityConversation } from './city-conversation.model';
+
+@Injectable()
+export class CitiesService {
+  cityConversations: FirebaseListObservable<any[]>;
+
+  constructor(private http: Http, private af: AngularFireDatabase) {
+    this.cityConversations = af.list('cityConversations');
+  }
+
+  addCityConvo(newCityConvo: CityConversation) {
+    this.cityConversations.push(newCityConvo);
+  }
+
+  getCities() {
+    return this.cityConversations;
+  }
+
+}
--- src/app/saved-cities/saved-cities.component.ts
+++ src/app/saved-cities/saved-cities.component.ts
@@ -2,20 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { CitiesApiService } from '../cities-api.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { CityConversation } from '../city-conversation.model';
+import { CitiesService } from '../cities.service';
 
 @Component({
   selector: 'app-saved-cities',
   templateUrl: './saved-cities.component.html',
   styleUrls: ['./saved-cities.component.css'],
-  providers: [CitiesApiService]
+  providers: [CitiesApiService, CitiesService]
 })
 export class SavedCitiesComponent implements OnInit {
   cities: FirebaseListObservable<any[]>;
 
-  constructor(private cityService: CitiesApiService) { }
+  constructor(private cityApiService: CitiesApiService, private citiesService: CitiesService) { }
 
   ngOnInit() {
-    this.cities = this.cityService.getCityConversations();
+    this.cities = this.citiesService.getCities();
   }
 
 }
